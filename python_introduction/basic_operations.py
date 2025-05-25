2c9e7628dc5f7fbafe6da3cac0518af14c9cd52b
Befkadu-S <befkadu.sahilu@aastustudent.edu.et>
2025-05-25 15:03:40 +0300

    Add 5 Python projects to python_introduction directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,4 @@
+number1 = 10
+number2 = 5
+
+print("Addition of 10 and 5 is ", number1+number2, "\nSubtraction of 10 and 5 is ", number1-number2, "\nMultiplication of 10 and 5 is ", number1*number2)
